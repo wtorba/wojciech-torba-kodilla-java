c75d7d959b667d486393b6a46e2c801bf5644faf
Wojciech Torba <wtorba@gmail.com>
2020-12-17 00:26:49 +0100

    9.4 Food2Door fix GlutenFreeShop name for test purpose

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/food2door/GlutenFreeShop.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/food2door/GlutenFreeShop.java
@@ -1,11 +1,9 @@
 package com.kodilla.good.patterns.food2door;
 
-import com.kodilla.good.patterns.food2door.Product;
-
 public class GlutenFreeShop implements Shop{
     public OrderDto process(OrderRequest orderRequest) {
         boolean isOrdered=false;
-        if (orderRequest.getSupplier().equals("GlutenFree") && orderRequest.getProduct().getProductName().matches("GlutenFree.*")) {
+        if (orderRequest.getSupplier().equals("GlutenFreeShop") && orderRequest.getProduct().getProductName().matches("GlutenFree.*")) {
             isOrdered=true;
         }
         return new OrderDto(orderRequest.getSupplier(),orderRequest.getProduct(), orderRequest.getQty(), isOrdered);
